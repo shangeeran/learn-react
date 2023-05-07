5156a8538908864adf916eee0b605f3619d57a94
shangeeran <shangeeran@gmail.com>
2023-05-08 03:16:03 +0530

    Did components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './components/Hello';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Hello/>
+      <Message/>
+
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello world!
@@ -17,7 +23,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
     </div>
   );
 }
---
+++ src/components/Hello.js
@@ -0,0 +1,11 @@
+// Functional components
+
+// Methode 1
+function Hello() {
+    return <h1>Hello World!</h1>
+};
+
+// Methode 1 (ES6)
+// const Hello = ()=> <h1>Hello medam</h1>
+
+export default Hello;
