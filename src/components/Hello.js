e14a50b27ba1c50065715895258b5a8f50620cee
shangeeran <shangeeran@gmail.com>
2023-05-08 03:30:03 +0530

    Did JSX

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,9 +1,20 @@
 // Functional components
 
-// Methode 1
+const name = "Shangeeran"
+const a = 15
+const displayMessage = () => {
+    return "I need a help!";
+}
+
 function Hello() {
-    return <h1>Hello World!</h1>
-};
+    return <h1>It says : {displayMessage()}</h1>
+}
+
+// Methode 1
+// function Hello() {
+//     return <h1>Hello World! {name}</h1>,
+//     <h2>Total is {a + 10}</h2>
+// };
 
 // Methode 1 (ES6)
 // const Hello = ()=> <h1>Hello medam</h1>
